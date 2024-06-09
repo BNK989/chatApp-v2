c450ca6649debbda52d0b58285c0176dc10addfd
Ben Klinovsky <bnk989@gmail.com>
2024-06-09 12:35:07 +0300

    fix date on msg issue

--- src/cmps/MsgItem.jsx
+++ src/cmps/MsgItem.jsx
@@ -1,12 +1,16 @@
-import { User } from '@/models/user.model'
+// import { User } from '@/models/user.model'
+// import { Message } from '@/models/chat.model'
 import { QuickAvatar } from './QuickAvatar'
 import { isRTL } from '@/lib/utils'
-import { Message } from '@/models/chat.model'
-export function MsgItem({ msg, user, isMe }: { msg: Message; user: User; isMe: boolean }) {
-    const createdAt: string = new Date(msg.createdAt).toLocaleTimeString()
+export function MsgItem({ msg, user, isMe }){//: { msg: Message; user: User; isMe: boolean }) {
 
-    // const createdAt = msg.createdAt.toDate().toLocaleTimeString();
-    // const createdAt = msg.createdAt.toDate().toLocaleTimeString()
+
+    const timeStamp = (msg.createdAt.seconds * 1000) + (msg.createdAt.nanoseconds / 1000000)
+
+    // const createdAt: string = new Date(msg.createdAt).toLocaleTimeString()
+    const createdAt = new Date(timeStamp).toLocaleTimeString()
+    console.log('isMe:', isMe)
+
     const isRtl = isRTL(msg.text)
 
 
@@ -18,7 +22,7 @@ export function MsgItem({ msg, user, isMe }: { msg: Message; user: User; isMe: b
                     <img className="w-2/4 object-cover rounded my-1 " src={msg.img} alt={user.username + 's image'} />
                 )}
                 <p dir={`${isRtl ? 'rtl' : 'ltr'}`} className=" p-3 rounded bg-cyan-950 bg-opacity-30">{msg.text}</p>
-                <span className="text-xs mt-1">{createdAt}</span>
+                <span className="text-xs mt-1">{createdAt|| 'noDate'}</span>
             </div>
         </div>
     )
